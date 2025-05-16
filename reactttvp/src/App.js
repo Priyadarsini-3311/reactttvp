e6e5ee8075051b69a4ee20ff0f402138923eed17
Priyadarsini-3311 <pdarsini331177@gmail.com>
2025-05-16 13:36:56 +0530

    program

---
+++ reactttvp/src/App.js
@@ -0,0 +1,39 @@
+function App()
+{
+  return(
+    <div>
+    <h1>Welcome to React first App</h1>
+    <h1>Welcome to React first App</h1>
+    <b>this is bold text</b>
+    <strike>this is strike text</strike>
+    <p> klsdf sadjfsal kjfsadkl <mark>asdklfasd jklfjasd flasdjf asldkjf sadlkjf sadkljfsdalkfdsakl fjasdklf asdklj fsadklf dsakl fskla faskdlf sad fsalkdf asdf skaldf </mark>sda</p>
+    </div>
+  )
+}
+export default App
+/*import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App
+*/
